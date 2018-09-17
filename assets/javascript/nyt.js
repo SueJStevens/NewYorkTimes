6c4fccfbc10081ba856c3fdf7ef295fe358a8646
Manny Mendoza <manny17t@gmail.com>
2018-09-16 23:16:59 -0700

    API query updated. Pushing results to HTML

--- assets/javascript/nyt.js
+++ assets/javascript/nyt.js
@@ -5,45 +5,59 @@ $(function () {
 
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     //set vars
-    var searchTerms = $('#searchTerms').val();
+    var searchTerms = $('#searchTerms').val().trim();
     var listAmount = $('#listAmount').val();
     var startYear  = $('#startYear').val();
     var endYear = $('#endYear').val();
-    console.log("Search Terms: "+ searchTerms);
-    console.log("List Amount: " + listAmount);
-    console.log("Start Year: " + startYear);
-    console.log(" End Year: "+ endYear);
+    // console.log("Search Terms: "+ searchTerms);
+    // console.log("List Amount: " + listAmount);
+    // console.log("Start Year: " + startYear);
+    // console.log(" End Year: "+ endYear);
+
+    // Cache HTML results div
+    var results_div = $('#results')
     
     
-    var startYear  = startYear + "0101";
-    var endYear = endYear + "1231";
+    var startYear  = startYear += "0101";
+    var endYear = endYear += "1231";
     
-    console.log(startYear);
-    console.log(endYear);
+    // console.log(startYear);
+    // console.log(endYear);
     
     var pageResults = Math.ceil(listAmount / 10);
     
     //Set
     
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
       'api-key': "582e6371a2b04467afd0ea36e7dd1e35",
       'q': searchTerms,
       'begin_date': startYear,
       'end_date': endYear,
-      'page': pageResults
-    
+      'page': 0
     });
-    console.log(url);
+    // console.log(searchTerms)
     $.ajax({
       url: url,
       method: 'GET',
-    }).done(function(result) {
-      console.log(result);
-    }).fail(function(err) {
+    }).then(function (result) {
+      
+      // Store array object in a variable
+      let articles = result.response.docs
+      // console.log(result.response)
+      console.log(articles);
+
+      // Push results to HTML
+      $.each(articles, function (i, article) {
+        console.log('article result: ' + article.headline.main)
+        results_div.append('<div class="result-div"><h1>' + article.headline.main + '</h1><p>Written by: ' + article.byline.original + '</p><p>Published: ' + article.pub_date + '</p><p>Summary: ' + article.snippet + '</p><a href="' + article.web_url + '">Click for article</a>')
+      })
+
+
+    }).fail(function (err) {
       throw err;
     });
-    
-     
+
 
   }); //end on-click button 
 
